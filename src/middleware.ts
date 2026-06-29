ad9c37f5f01d399b42811b45d00bb8aba5a105fb
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2026-06-29 12:40:38 +0530

    Auth middleware refactor for sitemap, and robots

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,8 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/inngest(.*)',
+  '/sitemap.xml',
+  '/robots.txt',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
